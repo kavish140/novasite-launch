98be8f1cae85d4834cb6193d9b7c5fa71fc2e720
Kavish Ganatra <kavish140@github.com>
2026-06-15 21:44:06 +0530

    perf: dynamically import sentry to unblock main thread and drastically improve FCP

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,20 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import * as Sentry from "@sentry/react";
-
-Sentry.init({
-  dsn: "https://2159c483e3c50155f15c45caf6fb3667@o4511547708997632.ingest.us.sentry.io/4511547730690048",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  tracesSampleRate: 1.0,
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
+if (import.meta.env.PROD) {
+  import("@sentry/react").then((Sentry) => {
+    Sentry.init({
+      dsn: "https://2159c483e3c50155f15c45caf6fb3667@o4511547708997632.ingest.us.sentry.io/4511547730690048",
+      integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration(),
+      ],
+      tracesSampleRate: 1.0,
+      replaysSessionSampleRate: 0.1,
+      replaysOnErrorSampleRate: 1.0,
+    });
+  });
+}
 
 createRoot(document.getElementById("root")!).render(
 	<ThemeProvider
